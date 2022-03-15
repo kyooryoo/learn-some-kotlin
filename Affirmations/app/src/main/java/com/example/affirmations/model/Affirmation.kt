8bb5491f42f11a47e405bed42e98a9c2de544fae
Ling Jiang <kyooryoo@hotmail.com>
2022-03-15 20:48:07 +0800

    update gitignore

---
+++ Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,10 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    // use arg comment to distinguish different types of args
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    )
